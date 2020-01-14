043e45569360f4ffb52c488e073509d8f2b7c1c2
Samuel Suh <samsuh@gmail.com>
2020-01-14 14:44:31 -0800

    added some more comments

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,14 +1,17 @@
-//process requests here; pull in request object and response object, run some logic and ultimately respond to it. 
-//POST requests to the /signup route runs the Authentication.signup function from here. 
+//process requests here; pull in request object and response object, run some logic and ultimately respond to it.
+//POST requests to the /signup route runs the Authentication.signup function from here.
 
 const User = require("../models/user");
 exports.signup = function(req, res, next) {
   //test route. use Postman to send a POST request to /signup route
   //res.send({success:'true'})
-  
-  //pull data out of request object. req.body from the 'post' request. req object has the data about the incoming request. 
+
+  //pull data out of request object. req.body from the 'post' request. req object has the data about the incoming request.
   //POST requests have a req.body property which is an empty object by default. if we POST JSON data along, it POSTS as req.body (JSON format expected; email/pw/company/jobTitle will be  passed along)
-  console.log("req.body is: ", req.body);
+  console.log(
+    "req.body from authentication.js in the signup function is: ",
+    req.body
+  );
   const email = req.body.email;
   const password = req.body.password;
   const company = req.body.company;
@@ -16,8 +19,10 @@ exports.signup = function(req, res, next) {
 
   //guarding against case where user does not send email or password from server-side before .save called on record
   // TO DO: can add server-side regex validation of email syntax
-  if(!email||!password){
-    return res.status(422).send({error: 'You must provide email and password'})
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "You must provide email and password" });
   }
 
   //See if user with given email exists; search all users for a matching email, return an error, or return existingUser (which is either null or contains user)
@@ -27,24 +32,27 @@ exports.signup = function(req, res, next) {
     if (err) {
       return next(err);
     }
-    //if a user with email DOES exists, return error
+    //if a user with email DOES exists in Mongo, return error
     if (existingUser) {
       return res.status(422).send({ error: "email is in use" });
     }
-    //if user with email DOES NOT exist, create a new instance of the User model, creating the record. not yet saved to db until the .save 
+    //if user with email DOES NOT exist, create a new instance of the User model, creating the record. not yet saved to db until the .save
     const user = new User({
       email: email,
       password: password,
-      company: company, 
-      jobTitle: jobTitle
+      company: company,
+      jobTitle: jobTitle,
     });
     user.save(function(err) {
       return next(err);
     });
-    //respond to request indicating the user was created. 
-    //test response: sending back the full user record for now. bad because sending back their pw too. 
+    //respond to request indicating the user was created in Mongo.
+    //test response: sending back the full user record for now to Express. dont actually use this because it's bad to send back their pw too.
     // res.json(user);
-    //send back a success so we're not sending back the full user record. 
-    res.json({success: true})
+    //send back a success so we're not sending back the full user record.
+    res.json({ success: true });
+    console.log(
+      "returned success:true to express, should next send the same message to ACS"
+    );
   });
 };
