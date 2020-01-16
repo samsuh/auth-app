133d45e15f386d5eb90db804fc5d200628865846
Samuel Suh <samsuh@gmail.com>
2020-01-15 16:21:05 -0800

    add cors module to allow cross origin resource requests from the react-client front end on a different port.

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express();
 const router = require("./router");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+//import cors module to allow cross-origin requests from the react front end running on a different port 
+const cors = require('cors')
 
 //DB Setup connecting mongoose with instance of mongodb (currently on mongodb atlas)
 mongoose.connect(keys.mongoURI);
@@ -16,8 +18,10 @@ mongoose.connect(keys.mongoURI);
 //morgan and bodyParser are middleware in express.
 //morgan is a logging framework, logs for debugging
 //bodyParser parses incoming requests as JSON. might present problems later for transfering files.
+//RECONFIGURE WHEN GOING TO PRODUCTION: cors allows requests from specific subdomains, but we're leaving it open for now. narrow down later.
 app.use(morgan("combined"));
 app.use(bodyParser.json({ type: "*/*" }));
+app.use(cors())
 router(app);
 
 //Server Setup, Express talking to the outside world
